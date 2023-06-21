1673eeb6e4b2dd47c1350d423f6dae4816f6b6dd
BhaskarUnguturu <bhaskarunguturu@gmail.com>
2023-06-21 15:05:27 -0500

    Food controllor implementation

---
+++ src/main/java/com/surplus/fwm/dto/FoodDto.java
@@ -0,0 +1,70 @@
+package com.surplus.fwm.dto;
+
+import java.sql.Date;
+
+public class FoodDto {
+	private String typeOfFood;
+	private int quantity;
+	private Date expirationDate;
+	private String dietaryRestrictions;
+	private Date schedulingTimeDate;
+	private String venue;
+	private String typeOfDonation;
+
+	public String getTypeOfFood() {
+		return typeOfFood;
+	}
+
+	public void setTypeOfFood(String typeOfFood) {
+		this.typeOfFood = typeOfFood;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public Date getExpirationDate() {
+		return expirationDate;
+	}
+
+	public void setExpirationDate(Date expirationDate) {
+		this.expirationDate = expirationDate;
+	}
+
+	public String getDietaryRestrictions() {
+		return dietaryRestrictions;
+	}
+
+	public void setDietaryRestrictions(String dietaryRestrictions) {
+		this.dietaryRestrictions = dietaryRestrictions;
+	}
+
+	public Date getSchedulingTimeDate() {
+		return schedulingTimeDate;
+	}
+
+	public void setSchedulingTimeDate(Date schedulingTimeDate) {
+		this.schedulingTimeDate = schedulingTimeDate;
+	}
+
+	public String getVenue() {
+		return venue;
+	}
+
+	public void setVenue(String venue) {
+		this.venue = venue;
+	}
+
+	public String getTypeOfDonation() {
+		return typeOfDonation;
+	}
+
+	public void setTypeOfDonation(String typeOfDonation) {
+		this.typeOfDonation = typeOfDonation;
+	}
+
+}
